5daf9fdfe7c0305764f2f97ec32e4b90e55093a8
parukrev <xavierrevian@gmail.com>
2024-11-07 15:43:09 +0700

    set Home (mobile, tab)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,64 @@
+import playIcon from "../assets/icons/circled-play.png";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="bg-primary text-white h-screen">
+      <header className="container py-7 relative z-10">
+        <nav className="flex justify-between">
+          <div>
+            <a href="/" className="font-semibold text-base md:text-lg leading-4">
+              Dreamcatcher
+            </a>
+          </div>
+          <div>
+            <button className="font-semibold text-sm">Menu</button>
+          </div>
+        </nav>
+      </header>
+      {/* BG Image */}
+      <div className="absolute inset-0 bg-hero-banner bg-contain bg-no-repeat bg-center opacity-30 grayscale"></div>
+      {/* Content */}
+      <div className="absolute inset-0 container flex flex-col justify-center">
+        <div>
+          <div className="flex justify-between uppercase text-accent font-medium text-sm md:text-base">
+            <p>dream</p>
+            <p>of</p>
+            <p>me</p>
+          </div>
+          <div>
+            <h1 className="uppercase text-[2.75rem] md:text-[5.45rem] font-bold text-center">
+              dreamcatcher
+            </h1>
+          </div>
+          <div className="flex justify-center gap-10 items-center mt-3">
+            <div>
+              <a href="#" className="flex items-center uppercase text-base md:text-2xl font-medium tracking-tight gap-2.5">
+                <img src={playIcon} alt="icon play" className="w-10 md:w-16" />
+                justice
+              </a>
+            </div>
+            <div className="flex items-end gap-4">
+              <p className="text-accent text-3xl md:text-5xl font-bold">VirtuouS</p>
+              <p className="text-xs md:text-base mb-[5px]">10th Mini Album</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Footer */}
+      <div className="absolute bottom-8 md:bottom-10 container">
+        <div className="flex justify-between items-end">
+          <div>
+            <p className="text-sm">&copy; 2024 Dreamcatcher Company. All Rights Reserved</p>
+          </div>
+          <div className="w-1/3">
+            <p className="text-sm font-semibold leading-5">In Dreamcatcher's story and lore, each member represent a different nightmare or fear. Diokophobia, Merinthophobia, Claustrophobia, Scopophobia, Mazeophobia, Agliophobia, Basophobia</p>
+          </div>
+          <div className="md:hidden">
+            <p className="text-accent text-md">&copy; 2024</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,45 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          sm: '428px',
+          md: '780px',
+          lg: '970px',
+          xl: '1350px',
+          '2xl': '1830px',
+        },
+      },
       fontFamily: {
         "main": ["Inter Tight", "sans-serif"]
+      },
+      colors: {
+        "primary": "#111111",
+        "accent": "#E10000",
+        "white": "#FFFFFF",
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              'font-size': '11.875rem',
+              'font-weight': '700',
+            },
+            h2: {
+              'font-size': '10.25rem',
+              'font-weight': '700',
+            },
+            h3: {
+              'font-size': '5.75rem',
+              'font-weight': '700',
+            }
+          }
+        }
+      }),
+      backgroundImage: {
+        'hero-banner': "url('/src/assets/img/banner.webp')",
       }
     },
   },
