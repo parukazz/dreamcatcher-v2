3271693354aba4a9d688771e8fb9f7933a64e65f
Paruk Azziyi <parukazziyi@gmail.com>
2024-11-10 19:49:14 +0700

    create constant/index file and add listmenu object

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+// list menu
+export const listMenu = [
+  { id: 1, label: "profile", link: "/profile" },
+  { id: 2, label: "news", link: "/news" },
+  { id: 3, label: "discography", link: "/discography" },
+  { id: 4, label: "video", link: "/video" },
+  { id: 5, label: "schedule", link: "/schedule" },
+  { id: 6, label: "contact", link: "/contact" },
+];
