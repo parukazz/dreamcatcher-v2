f5a7ef95de4c212736fdebb4ebcb0e10ba5b3a9e
parukrev <xavierrevian@gmail.com>
2024-11-18 15:31:37 +0700

    create about page until menu, group name & photo group

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,29 @@
 import Navbar from "../components/Navbar";
+import { members } from "../constant";
+import mainPhoto from "../assets/img/profile-group.webp";
 
 const About = () => {
   return (
-    <main>
-      <div>
-        <Navbar theme={"bg-white text-primary"} />
+    <main className="bg-white">
+      <Navbar theme={"bg-white text-primary"} />
+      <div className="container">
+        <div className="text-center mb-12">
+          <h1 className="uppercase font-extrabold text-[44px] tracking-tight mb-6">
+            dreamcatcher
+          </h1>
+          <div className="flex flex-wrap gap-x-9 gap-y-7 justify-center">
+            {members.map((member) => (
+              <div key={member.id}>
+                <p className="uppercase font-bold text-sm tracking-tight">
+                  {member.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="">
+          <img src={mainPhoto} alt="Group Photo" className="w-full" />
+        </div>
       </div>
     </main>
   );
