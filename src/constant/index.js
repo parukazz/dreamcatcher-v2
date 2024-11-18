e60471e458dec2d705ea42bdc0f6c02c38f39a69
parukrev <xavierrevian@gmail.com>
2024-11-18 15:31:02 +0700

    add members list

--- src/constant/index.js
+++ src/constant/index.js
@@ -7,3 +7,14 @@ export const listMenu = [
   { id: 5, label: "schedule", link: "/schedule" },
   { id: 6, label: "contact", link: "/contact" },
 ];
+
+// Members name
+export const members = [
+  { id: 1, name: "ji u" },
+  { id: 2, name: "su a" },
+  { id: 3, name: "siyeon" },
+  { id: 4, name: "handong" },
+  { id: 5, name: "yoohyeon" },
+  { id: 6, name: "dami" },
+  { id: 7, name: "gahyeon" },
+];
