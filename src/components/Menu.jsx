ed517b24b0e59b90e8a188a574dce7eb600dd3dd
parukrev <xavierrevian@gmail.com>
2024-11-20 14:03:44 +0700

    setting theme props

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ const Menu = ({ theme }) => {
   return (
     <div
       id="menuBox"
-      className={`absolute inset-0 bg-primary text-white z-10 ${theme}`}
+      className={`absolute inset-0 bg-primary z-10 ${theme}`}
     >
       <ul className="w-full h-full uppercase font-bold text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl flex flex-col justify-center items-center gap-1 md:gap-4 lg:gap-3">
         {listMenu.map((item) => (
